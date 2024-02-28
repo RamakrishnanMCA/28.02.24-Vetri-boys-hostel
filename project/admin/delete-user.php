6151a66dee7373eed57b5907ec5433197e05f840
Ramakrishnan M <147298409+RamakrishnanMCA@users.noreply.github.com>
2024-02-28 07:46:32 +0530

    Delete project directory

--- project/admin/delete-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    mysqli_query($conn, "DELETE FROM users WHERE id=$id");
-}
-
-header('Location: admin_manageuser.php');
